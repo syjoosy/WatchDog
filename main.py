108c880efe76c773e650cf25211de08d8df8541f
syjoosy <64636699+syjoosy@users.noreply.github.com>
2020-08-29 23:53:46 +0300

    fixes

--- main.py
+++ main.py
@@ -3,56 +3,63 @@ import os
 import time
 # FileSystemEventHandler - класс по отслеживанию изменений
 from watchdog.events import FileSystemEventHandler
-from tkinter import *
-from tkinter import filedialog as fd
+# from tkinter import *
+# from tkinter import filedialog as fd
 
-window =Tk()
-window.title("WatchDog by syjoosy")
-window.geometry('800x800')
+# window = Tk()
+# window.title("WatchDog by syjoosy")
+# window.geometry('800x800')
+# lbl = Label(window, text="Список всех фильтруемых расширений:", font=("Arial Bold", 16))
+# lbl.grid(column=0, row=0)
 
-lbl = Label(window, text="Список всех фильтруемых расширений:", font=("Arial Bold", 16))
-lbl.grid(column=0, row=0)
+# def extractText():
+#     file_name = fd.asksaveasfilename(filetypes=(("TXT files", "*.txt"),
+# 												("HTML files", "*.html;*.htm"),
+#                                                 ("All files", "*.*") ))
+#     f = open(file_name, 'w')
+#     s = text.get(1.0, END)
+#     f.write(s)
+#     f.close()
 
-def extractText():
-    file_name = fd.asksaveasfilename(filetypes=(("TXT files", "*.txt"),
-                                        ("HTML files", "*.html;*.htm"),
-                                                ("All files", "*.*") ))
-    f = open(file_name, 'w')
-    s = text.get(1.0, END)
-    f.write(s)
-    f.close()
-
-def append():
-	arr.append(txt.get())
-	lbox.insert(END, txt.get())
-	txt.delete(0, END)
+# def append():
+# 	arr.append(txt.get())
+# 	lbox.insert(END, txt.get())
+# 	txt.delete(0, END)
 
 arr = ["jpg", "png", "svg", "jpeg", "mp4", "mkv", "zip", "rar", "deb", "doc", "docx", "pdf", "odt", "txt", "py"]
-lbox = Listbox()
-for i in arr:
-	lbox.insert(END,i)
-lbox.place(x=10, y=40)
+# lbox = Listbox()
+# for i in arr:
+# 	lbox.insert(END,i)
+# lbox.place(x=10, y=40)
 
-lbl = Label(window, text="Добавить расширение:", font=("Arial Bold", 16))
-lbl.place(x=10, y=240)
+# lbl = Label(window, text="Название:", font=("Arial Bold", 16))
+# lbl.place(x=10, y=240)
 
-txt = Entry(window, width=10)
-txt.place(x=280, y=245)
+# txt1 = Entry(window, width=10)
+# txt1.place(x=280, y=245)
 
-lbl = Label(window, text="Куда сортировать:", font=("Arial Bold", 16))
-lbl.place(x=10, y=280)
+# lbl = Label(window, text="Добавить расширение:", font=("Arial Bold", 16))
+# lbl.place(x=10, y=280)
 
-text = Entry(window, width=10)
-text.place(x=280, y=285)
+# txt = Entry(window, width=10)
+# txt.place(x=280, y=285)
 
-btn = Button(window, text="Добавить", command=append)
-btn.place(x=380, y=280)
+# lbl = Label(window, text="Куда сортировать:", font=("Arial Bold", 16))
+# lbl.place(x=10, y=320)
+
+# text = Entry(window, width=10)
+# text.place(x=280, y=325)
+
+# btn = Button(window, text="Добавить", command=append)
+# btn.place(x=380, y=320)
 
 # Создаем класс наследник, через него может отслеживать изменения в папках
+kolvo = 0
 class Handler(FileSystemEventHandler):
 	# При любых изменениях в папке, мы перемещаем файлы в ней
 	def on_modified(self, event):
 		# Перебираем все файлы в папке folder_track
+		global kolvo
 		for filename in os.listdir(folder_track):
 			# Проверяем расширенеи файла
 			extension = filename.split(".")
@@ -63,6 +70,7 @@ class Handler(FileSystemEventHandler):
 				folder_dest = '/home/syjoosy/Изображения/'
 				new_path = folder_dest + filename
 				os.rename(file, new_path)
+				kolvo = kolvo + 1
 			# Если файл видео, то в папку с видео
 			# Такое же можно прописать и для других расширений файлов
 			if len(extension) > 1 and (extension[1].lower() == arr[4] or extension[1].lower() == arr[5]):
@@ -70,26 +78,32 @@ class Handler(FileSystemEventHandler):
 				folder_dest = '/home/syjoosy/Видео/'
 				new_path = folder_dest + filename
 				os.rename(file, new_path)
+				kolvo = kolvo + 1
 			if len(extension) > 1 and (extension[1].lower() == arr[6] or extension[1].lower() == arr[7]):
 				file = folder_track + "/" + filename
 				folder_dest = '/home/syjoosy/Загрузки'
 				new_path = folder_dest + "/Zip_Rar/" + filename
 				os.rename(file, new_path)
+				kolvo = kolvo + 1
 			if len(extension) > 1 and extension[1].lower() == arr[8]:
 				file = folder_track + "/" + filename
 				folder_dest = '/home/syjoosy/Загрузки'
 				new_path = folder_dest + "/Deb/" + filename
 				os.rename(file, new_path)
+				kolvo = kolvo + 1
 			if len(extension) > 1 and (extension[1].lower() == arr[9] or extension[1].lower() == arr[10] or extension[1].lower() == arr[11] or extension[1].lower() == arr[12] or extension[1].lower() == arr[13]):
 				file = folder_track + "/" + filename
 				folder_dest = '/home/syjoosy/Документы/'
 				new_path = folder_dest + filename
 				os.rename(file, new_path)
+				kolvo = kolvo + 1
 			elif len(extension) > 1 and extension[1].lower() == arr[14]:
 				file = folder_track + "/" + filename
 				folder_dest = '/home/syjoosy/Загрузки'
 				new_path = folder_dest + "/PyDoc/" + filename
 				os.rename(file, new_path)
+				kolvo = kolvo + 1
+		print("Перемещенно объектов: ", kolvo)
 	# def read(self):
 	# 	print("FUNCTION")
 	# 	f = open('settings.txt')
@@ -106,7 +120,8 @@ handle = Handler()
 observer = Observer()
 observer.schedule(handle, folder_track, recursive=True)
 observer.start()
-window.mainloop()
+print("Старт...")
+# window.mainloop()
 # Программа будет срабатывать каждые 10 милисекунд
 try:
 	while(True):
