ddec7f4dcff3a07ed3b51afc6f12fec6011e78b1
syjoosy <64636699+syjoosy@users.noreply.github.com>
2020-09-01 23:36:07 +0300

    graphical interface fixes, added new functions

--- main.py
+++ main.py
@@ -3,57 +3,130 @@ import os
 import time
 # FileSystemEventHandler - класс по отслеживанию изменений
 from watchdog.events import FileSystemEventHandler
-# from tkinter import *
+from tkinter import *
 # from tkinter import filedialog as fd
 
-# window = Tk()
-# window.title("WatchDog by syjoosy")
-# window.geometry('800x800')
-# lbl = Label(window, text="Список всех фильтруемых расширений:", font=("Arial Bold", 16))
-# lbl.grid(column=0, row=0)
 
-# def extractText():
-#     file_name = fd.asksaveasfilename(filetypes=(("TXT files", "*.txt"),
-# 												("HTML files", "*.html;*.htm"),
-#                                                 ("All files", "*.*") ))
-#     f = open(file_name, 'w')
-#     s = text.get(1.0, END)
-#     f.write(s)
-#     f.close()
+window = Tk()
+window.title("WatchDog by syjoosy")
+window.geometry('800x800')
+lbl = Label(window, text="Список всех фильтруемых расширений:", font=("Arial Bold", 16))
+lbl.grid(column=0, row=0)
+
+
+
+arr = ["jpg", "png", "svg", "jpeg", "mp4", "mkv", "zip", "rar", "deb", "doc", "docx", "pdf", "odt", "txt", "py"]
 
 # def append():
 # 	arr.append(txt.get())
 # 	lbox.insert(END, txt.get())
 # 	txt.delete(0, END)
 
-arr = ["jpg", "png", "svg", "jpeg", "mp4", "mkv", "zip", "rar", "deb", "doc", "docx", "pdf", "odt", "txt", "py"]
-# lbox = Listbox()
-# for i in arr:
-# 	lbox.insert(END,i)
-# lbox.place(x=10, y=40)
+def read(filename="settings.txt"):
+	try:
+		# filename = "settings.txt"
+		filehandle = open(filename, 'r')
+		i = 0
+		while True:
+			line = filehandle.readline()
+			if not line:
+				break
+			i += 1
+		filehandle.close()
+		print("Весь файл успешно прочтен, {} расширений".format(i))
+	except BaseException:
+		print("Ошибка чтения файла")
 
-# lbl = Label(window, text="Название:", font=("Arial Bold", 16))
-# lbl.place(x=10, y=240)
 
-# txt1 = Entry(window, width=10)
-# txt1.place(x=280, y=245)
 
-# lbl = Label(window, text="Добавить расширение:", font=("Arial Bold", 16))
-# lbl.place(x=10, y=280)
+def write(filename="settings.txt"):
+	i = 0
+	try:
+		with open(filename, "w") as file:
+			for line in arr:
+				file.write(line + '\n')
+				i += 1
 
-# txt = Entry(window, width=10)
-# txt.place(x=280, y=285)
+		print("Запись прошла успешно, добавлено {} расширений".format(i))
+	except BaseException:
+		print("Ошибка записи файла")
 
-# lbl = Label(window, text="Куда сортировать:", font=("Arial Bold", 16))
-# lbl.place(x=10, y=320)
+def new(filename="settings.txt"):
+	try:
+		txt.config(highlightbackground="black", highlightcolor="black")
+		res = txt.get()
+		# txt.delete(1.0, END)
+		# lbl.configure(text=res)
+		# expansion = input("Введите новое расширение: ")
+		# arr.append(expansion)
+		arr.append(res)
+		lbox.insert(END, res)
+		with open(filename, "a") as myfile:
+			myfile.write("\n"+res)
+		txt.config(highlightbackground="lawn green", highlightcolor="lawn green")
+		print("Расширение успешно добавлено")
+	except BaseException:
+		txt.config(highlightbackground="red", highlightcolor="red")
+		print("Ошибка, массив расширений не найден")
 
-# text = Entry(window, width=10)
-# text.place(x=280, y=325)
+def newtype():
+	# name = input("Введите название категории: ")
+	res = txt1.get()
+	# txt1.delete(1.0, END)
+	myFile = open(res, "w")
+	txt1.config(highlightbackground="lawn green", highlightcolor="lawn green")
+	write(res)
+	read(res)
+	myFile.close()
 
-# btn = Button(window, text="Добавить", command=append)
-# btn.place(x=380, y=320)
+def extension():
+	try:
+		# read("photo.txt")
+		pass
+	except BaseException:
+		pass
+
+
+
+
+lbox = Listbox()
+for i in arr:
+	lbox.insert(END,i)
+lbox.place(x=10, y=40)
+
+lbl = Label(window, text="Название:", font=("Arial Bold", 16))
+lbl.place(x=10, y=240)
+
+txt1 = Entry(window, width=10)
+txt1.place(x=280, y=245)
+
+btn = Button(window, text="Добавить", command=newtype)
+btn.place(x=380, y=240)
+
+lbl = Label(window, text="Добавить расширение:", font=("Arial Bold", 16))
+lbl.place(x=10, y=280)
+
+txt = Entry(window, width=10)
+txt.place(x=280, y=285)
+
+# txt.pack()
+
+btn = Button(window, text="Добавить", command=new)
+btn.place(x=380, y=280)
+
+lbl = Label(window, text="Куда сортировать:", font=("Arial Bold", 16))
+lbl.place(x=10, y=320)
+
+text = Entry(window, width=10)
+text.place(x=280, y=325)
+
+btn = Button(window, text="Добавить", command=new)
+btn.place(x=380, y=320)
 
 # Создаем класс наследник, через него может отслеживать изменения в папках
+
+
+
 kolvo = 0
 class Handler(FileSystemEventHandler):
 	# При любых изменениях в папке, мы перемещаем файлы в ней
@@ -110,37 +183,11 @@ class Handler(FileSystemEventHandler):
 	# 	print(f.read())
 	# 	f.close()
 
-def read():
-	try:
-		filename = "settings.txt"
-		filehandle = open(filename, 'r')
-		i = 0
-		while True:
-			line = filehandle.readline()
-			if not line:
-				break
-			i += 1
-		filehandle.close()
-		print("Весь файл успешно прочтен, {} расширений".format(i))
-	except BaseException:
-		print("Ошибка чтения файла")
 
-
-def write():
-	i = 0
-	try:
-		with open(r"settings.txt", "w") as file:
-			for line in arr:
-				file.write(line + '\n')
-				i += 1
-		print("Запись прошла успешно, добавлено {} расширений".format(i))
-	except BaseException:
-		print("Ошибка записи файла")
-
-
-
-read()
-write()
+# new()
+# read()
+# write()
+# newtype()
 # Папка что отслеживается
 folder_track = '/home/syjoosy/Загрузки'
 # Папка куда перемещать будем
@@ -153,8 +200,8 @@ observer.schedule(handle, folder_track, recursive=True)
 observer.start()
 # print("\n")
 print("Старт...")
-# window.mainloop()
-# Программа будет срабатывать каждые 10 милисекунд
+window.mainloop()
+# Программа 	будет срабатывать каждые 10 милисекунд
 try:
 	while(True):
 		time.sleep(10)
